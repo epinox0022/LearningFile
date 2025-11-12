9bcb8e756348aac53f8142071556d62b5f030bb1
Jhonny Mark <fjhonnymark@gmail.com>
2025-11-12 21:05:33 +0800

    mark

--- HereLies1/mywork.js
+++ HereLies1/mywork.js
@@ -62,8 +62,14 @@ const arr = [1,2,3,4,5];
 arr.reverse().push(0);
 arr.unshift(6);
 
-
 console.log(arr);
+
+const arr1 = [1,2,3,4,5]
+const arr2 = [5,6,7,8,9,10]
+
+const arr3 = arr1.concat(arr2);
+
+console.log(arr3);
 
 
 /* */ //Use this for your comments and grouping
