b611efcc94bf0bc1a94eaeb2453046c364d4ea6c
Jhonny Mark <fjhonnymark@gmail.com>
2025-11-12 21:12:47 +0800

    mark

--- HereLies1/mywork.js
+++ HereLies1/mywork.js
@@ -58,19 +58,15 @@ alert(`Quiz finished! Your score: ${score}/${questions.length}`);
 */
 
 const arr = [1,2,3,4,5];
+arr.reverse().push(0) + arr.unshift(6);
 
-arr.reverse().push(0);
-arr.unshift(6);
-
-console.log(arr);
-
-const arr1 = [1,2,3,4,5]
-const arr2 = [5,6,7,8,9,10]
+const arr1 = [1,2,3,4,5];
+const arr2 = [5,6,7,8,9,10];
 
 arr1.splice(4);
-
 const arr3 = arr1.concat(arr2);
 
+console.log(arr);
 console.log(arr3);
 
 
